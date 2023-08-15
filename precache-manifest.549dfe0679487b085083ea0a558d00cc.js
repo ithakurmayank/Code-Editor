465e7b3fd1636d2df81c60509d22584451848914
Mayank <mt3197356@gmail.com>
2023-08-15 15:04:28 +0530

    Updates

---
+++ precache-manifest.549dfe0679487b085083ea0a558d00cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bb3383bea967b6fb089e41df54802c",
+    "url": "/Code-Editor/index.html"
+  },
+  {
+    "revision": "f6ea45b98e555abe2a7a",
+    "url": "/Code-Editor/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "9fc5fd9a7c70d49194e7",
+    "url": "/Code-Editor/static/css/main.588f3456.chunk.css"
+  },
+  {
+    "revision": "f6ea45b98e555abe2a7a",
+    "url": "/Code-Editor/static/js/2.7b424818.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/Code-Editor/static/js/2.7b424818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc5fd9a7c70d49194e7",
+    "url": "/Code-Editor/static/js/main.5526caf7.chunk.js"
+  },
+  {
+    "revision": "516eacaddb1afab27d95",
+    "url": "/Code-Editor/static/js/runtime-main.467bb05f.js"
+  }
+]);
